0224d49e6fc8d1032db51804087103ca10e5b579
Nmerced25 <101299252+Nmerced25@users.noreply.github.com>
2022-06-30 20:02:22 -0400

    Add files via upload

--- Charts.js
+++ Charts.js
@@ -1,10 +1,11 @@
 function init() {
   // Grab a reference to the dropdown select element
-    var selector = d3.select("#selDataset");
+    var selector = d3.select("samples.json");
   
   // Use the list of sample names to populate the select options
     d3.json("samples.json").then((data) => {
     var sampleNames = data.names;
+    
     sampleNames.forEach((sample) => {
         selector
           .append("option")
